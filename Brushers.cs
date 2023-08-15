4a7e1ce35d6a1f172b8673c74464f1b6b2b0f0b5
Wouter de Vries <wouterdevries@silverfit.nl>
2023-08-15 07:30:03 +0200

    kleur

---
+++ Brushers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfColorTheming
+{
+    public static class Brushers
+    {
+
+
+
+
+        public static Brush GetHoverBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HoverBrushProperty);
+        }
+
+        public static void SetHoverBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HoverBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HoverBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverBrushProperty =
+            DependencyProperty.RegisterAttached("HoverBrush", typeof(Brush), typeof(Brushers), new PropertyMetadata(null));
+
+
+
+
+        public static Brush GetActiveBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(ActiveBrushProperty);
+        }
+
+        public static void SetActiveBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(ActiveBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ActiveBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ActiveBrushProperty =
+            DependencyProperty.RegisterAttached("ActiveBrush", typeof(Brush), typeof(Brushers), new PropertyMetadata(null));
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace WpfColorTheming
         public MainWindow()
         {
             InitializeComponent();
-            string a = "text a";
         }
     }
 }
